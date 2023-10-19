aacf6d55a54fed1209f234d47f77bdcda8046aa6
yuyu5510 <yuyu5510.chara@icloud.com>
2023-10-19 22:58:56 +0900

    演習7の演習課題

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
 /* placeholder file for JavaScript */
+const confirm_delete = (id) => {
+  if (window.confirm(`Task ${id} を削除します。よろしいですか？`)) {
+    location.href = `/task/delete/${id}`;
+  }
+};
+
+const confirm_update = (id) => {
+  if (window.confirm(`Task ${id} を更新します。よろしいですか？`)) {
+    var form = document.getElementById(`edit-${id}`);
+    form.submit();
+  }
+};
